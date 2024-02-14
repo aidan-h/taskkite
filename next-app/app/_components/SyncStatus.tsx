ec418e9b4a355785f2200ab95a611289975248d9
aidan-h <aidanhammond2003@gmail.com>
2024-02-14 08:47:34 -0600

    Stylized sync status, closes #87

--- next-app/app/_components/SyncStatus.tsx
+++ next-app/app/_components/SyncStatus.tsx
@@ -4,15 +4,17 @@ import useProjects from "../_lib/useProjects";
 
 function N({ syncState, name }: { syncState: SyncState, name: string }) {
 	return <div>
-		syncing {name} {syncState == SyncState.FAILED ? "failed" : ""}
+		{name} {syncState == SyncState.FAILED ? "failed to update" : syncState == SyncState.SYNCING ? "is updating" : "updated"}
 	</div>
 }
 
 export default function SyncStatus() {
 	const projects = useProjects();
-	return <div className="absolute right-0 bottom-0">
-		{projects.filter((project) => project.state != SyncState.SYNCED).map((project) =>
-			<N key={project.client.data.id} name={project.client.data.name} syncState={project.state} />)}
+	const status = projects.filter((project) => project.state != SyncState.SYNCED).map((project) =>
+		<N key={project.client.data.id} name={project.client.data.name} syncState={project.state} />)
+	if (status.length == 0) return <></>
+	return <div className="absolute bg-indigo-500 m-5 p-2 rounded text-zinc-50 right-0 bottom-0">
+		{status}
 	</div>
 }
 
