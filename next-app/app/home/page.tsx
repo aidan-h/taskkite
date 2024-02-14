7b41fec8557c46f1297e57b19e0084b841d341a0
aidan-h <aidanhammond2003@gmail.com>
2024-02-14 08:47:33 -0600

    Added Google OAuth

---
+++ next-app/app/home/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { useRouter } from "next/navigation";
+
+interface Task {
+	title: string,
+	description?: string,
+	id: number,
+	createdAt: Date,
+	due?: Date,
+	labels?: string[]
+}
+
+
+function tomorrow() {
+	const tomorrow = new Date()
+	tomorrow.setTime(tomorrow.getTime() + 24 * 60 * 60 * 1000);
+	return tomorrow
+
+}
+
+const defaultTasks: Task[] = [
+	{ title: "Take out trash", id: 0, createdAt: new Date() },
+	{ title: "Feed dogs", id: 1, description: "Arnold is allergic to chicken", due: new Date(), createdAt: new Date() },
+	{ title: "Make dinner", id: 2, createdAt: new Date(), description: "Fried rice", due: tomorrow() }
+]
+
+interface Account {
+	tasks: Task[]
+}
+
+function sameDay(a: Date, b: Date): boolean {
+	return a.getFullYear() == b.getFullYear() && a.getMonth() == b.getMonth() && a.getDate() == b.getDate()
+}
+
+function todayTasks(account: Account): Task[] {
+	const today = new Date()
+	return account.tasks.filter((task) => task.due && sameDay(today, task.due))
+}
+
+function TaskWidget({ task }: { task: Task }) {
+	return <div className="bg-white rounded-xl max-w-sm m-4 p-3">
+		<h2 className="text-lg">{task.title}</h2>
+		<p className="text-slate-500">{task.description}</p>
+		<p>Created at {task.createdAt.toDateString()}</p>
+		{task.due ? <p>Due at {task.due.toDateString()}</p> : <></>}
+	</div>
+}
+
+function TaskList({ tasks }: { tasks: Task[] }) {
+	return <div>{tasks.map((task) => <TaskWidget key={task.id} task={task}></TaskWidget>)}</div>
+}
+
+export default function Home() {
+	const router = useRouter()
+	return (
+		<>
+			<TaskList tasks={defaultTasks}></TaskList>
+			<button onClick={() => router.push("/account")}>Account</button>
+		</>
+	)
+}
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,56 +1,20 @@
-interface Task {
-	title: string,
-	description?: string,
-	id: number,
-	createdAt: Date,
-	due?: Date,
-	labels?: string[]
-}
+'use client'
+import { useSession, signIn } from "next-auth/react"
+import { useRouter } from 'next/navigation'
 
+export default function Component() {
+	const { data: session } = useSession()
+	const router = useRouter()
+	if (session && session.user) {
+		router.push("/home")
+	}
+	const footnote = "100% original name and idea. Please don't steal!"
 
-function tomorrow() {
-	const tomorrow = new Date()
-	tomorrow.setTime(tomorrow.getTime() + 24 * 60 * 60 * 1000);
-	return tomorrow
-
-}
-
-const defaultTasks: Task[] = [
-	{ title: "Take out trash", id: 0, createdAt: new Date() },
-	{ title: "Feed dogs", id: 1, description: "Arnold is allergic to chicken", due: new Date(), createdAt: new Date() },
-	{ title: "Make dinner", id: 2, createdAt: new Date(), description: "Fried rice", due: tomorrow() }
-]
-
-interface Account {
-	tasks: Task[]
-}
-
-function sameDay(a: Date, b: Date): boolean {
-	return a.getFullYear() == b.getFullYear() && a.getMonth() == b.getMonth() && a.getDate() == b.getDate()
-}
-
-function todayTasks(account: Account): Task[] {
-	const today = new Date()
-	return account.tasks.filter((task) => task.due && sameDay(today, task.due))
-}
-
-function TaskWidget({ task }: { task: Task }) {
-	return <div className="bg-white rounded-xl max-w-sm m-4 p-3">
-		<h2 className="text-lg">{task.title}</h2>
-		<p className="text-slate-500">{task.description}</p>
-		<p>Created at {task.createdAt.toDateString()}</p>
-		{task.due ? <p>Due at {task.due.toDateString()}</p> : <></>}
-	</div>
-}
-
-function TaskList({ tasks }: { tasks: Task[] }) {
-	return <div>{tasks.map((task) => <TaskWidget key={task.id} task={task}></TaskWidget>)}</div>
-}
-
-export default function Home() {
 	return (
-		<main>
-			<TaskList tasks={defaultTasks}></TaskList>
-		</main>
+		<>
+			<h1>To-Do App</h1>
+			<button onClick={() => signIn()}>Sign in</button>
+			<p>{footnote}</p>
+		</>
 	)
 }
