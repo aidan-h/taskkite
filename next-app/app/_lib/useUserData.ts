3af7892aa44a764714eb8cfe38cf670e22b1d2a8
aidan-h <aidanhammond2003@gmail.com>
2024-02-14 08:47:33 -0600

    Formatted code with prettier

--- next-app/app/_lib/useUserData.ts
+++ next-app/app/_lib/useUserData.ts
@@ -1,83 +1,103 @@
-import { createContext, } from "react";
+import { createContext } from "react";
 import { getAppData, getProject, syncProject } from "./api";
-import { AppData, ClientEvent, CreateTaskEvent, DeleteTaskEvent, EditTaskEvent, Project } from "./data";
-import { IncrementalData, SyncClient, SyncStatus, useIncrementalData, useSyncClient } from "./sync";
+import {
+  AppData,
+  ClientEvent,
+  CreateTaskEvent,
+  DeleteTaskEvent,
+  EditTaskEvent,
+  Project,
+} from "./data";
+import {
+  IncrementalData,
+  SyncClient,
+  SyncStatus,
+  useIncrementalData,
+  useSyncClient,
+} from "./sync";
 
 export const AppDataContext = createContext<AppData>({
-	email: "test@test.com",
-	name: "test",
-	projects: [],
+  email: "test@test.com",
+  name: "test",
+  projects: [],
 });
 
 export const SyncStateContext = createContext<SyncStatus>(SyncStatus.PENDING);
 
 export type ProjectClientData = SyncClient<Project> & {
-	emitEvent: (event: ClientEvent) => void;
+  emitEvent: (event: ClientEvent) => void;
 };
 
 export type ClientData = SyncClient<AppData>;
 
 function applyEvent(project: Project, [name, data]: ClientEvent): Project {
-	if (name == "createTask") {
-		const event = data as CreateTaskEvent;
-		return {
-			...project,
-			tasks: [...project.tasks, {
-				name: event.name,
-				description: event.description,
-				labels: event.labels,
-				id: project.historyCount,
-			}],
-			historyCount: project.historyCount + 1,
-			taskCount: project.taskCount + 1,
-		}
-	} else if (name == "editTask") {
-		const event = data as EditTaskEvent;
-		let tasks = project.tasks.slice()
-		const index = tasks.findIndex((task) => task.id == event.id)
-		if (index == -1) {
-			console.error("couldn't apply event to unfound task", event, project);
-			return project;
-		}
+  if (name == "createTask") {
+    const event = data as CreateTaskEvent;
+    return {
+      ...project,
+      tasks: [
+        ...project.tasks,
+        {
+          name: event.name,
+          description: event.description,
+          labels: event.labels,
+          id: project.historyCount,
+        },
+      ],
+      historyCount: project.historyCount + 1,
+      taskCount: project.taskCount + 1,
+    };
+  } else if (name == "editTask") {
+    const event = data as EditTaskEvent;
+    let tasks = project.tasks.slice();
+    const index = tasks.findIndex((task) => task.id == event.id);
+    if (index == -1) {
+      console.error("couldn't apply event to unfound task", event, project);
+      return project;
+    }
 
-		tasks[index] = {
-			name: event.name,
-			description: event.description,
-			labels: event.labels,
-			archived: event.archived,
-			completed: event.completed,
-			id: event.id,
-		}
+    tasks[index] = {
+      name: event.name,
+      description: event.description,
+      labels: event.labels,
+      archived: event.archived,
+      completed: event.completed,
+      id: event.id,
+    };
 
-		return {
-			...project,
-			tasks: tasks,
-			historyCount: project.historyCount + 1,
-		}
-	} else if (name == "deleteTask") {
-		const event = data as DeleteTaskEvent;
-		return {
-			...project,
-			tasks: project.tasks.filter((task) => task.id != event.id),
-			historyCount: project.historyCount + 1,
-		}
-	} else {
-		console.error("couldn't apply event " + name);
-	}
-	return project;
+    return {
+      ...project,
+      tasks: tasks,
+      historyCount: project.historyCount + 1,
+    };
+  } else if (name == "deleteTask") {
+    const event = data as DeleteTaskEvent;
+    return {
+      ...project,
+      tasks: project.tasks.filter((task) => task.id != event.id),
+      historyCount: project.historyCount + 1,
+    };
+  } else {
+    console.error("couldn't apply event " + name);
+  }
+  return project;
 }
 
 export function useProjectData(
-	id: number,
+  id: number,
 ): IncrementalData<Project, ClientEvent> {
-	return useIncrementalData(
-		() => getProject({ projectId: id }),
-		(events, data) =>
-			syncProject({ projectId: data.id, changes: events, index: data.historyCount }),
-		applyEvent,
-	);
+  return useIncrementalData(
+    () => getProject({ projectId: id }),
+    (events, data) =>
+      syncProject({
+        projectId: data.id,
+        changes: events,
+        index: data.historyCount,
+      }),
+    applyEvent,
+  );
 }
 
 export default function useClientData(): ClientData {
-	return useSyncClient(getAppData);
+  return useSyncClient(getAppData);
 }
--- next-app/app/api/project/get/route.ts
+++ next-app/app/api/project/get/route.ts
@@ -9,48 +9,49 @@ SELECT id, name, description, archived, completed FROM task WHERE project_id = ?
 `;
 
 async function getProject(db: Connection, projectId: number): Promise<Project> {
-	const [rows,] = await db.execute<RowDataPacket[][]>(GET_PROJECT_STATEMENTS, [
-		projectId, projectId]);
-	const projectRow = rows[0][0] as {
-		name: string,
-		owner: string,
-		task_count: number,
-		history_count: number
-	} | undefined;
-	const taskRows = rows[1] as {
-		id: number,
-		name: string,
-		description: string,
-		archived: boolean,
-		completed: boolean,
-	}[];
-	if (projectRow == undefined)
-		throw "couldn't find project " + projectId
-	return {
-		id: projectId,
-		name: projectRow.name,
-		historyCount: projectRow.history_count,
-		taskCount: projectRow.task_count,
-		owner: projectRow.owner,
-		tasks: taskRows.map((row) => {
-			return {
-				name: row.name,
-				id: row.id,
-				archived: row.archived,
-				description: row.description,
-				completed: row.completed,
-			}
-		})
-	}
+  const [rows] = await db.execute<RowDataPacket[][]>(GET_PROJECT_STATEMENTS, [
+    projectId,
+    projectId,
+  ]);
+  const projectRow = rows[0][0] as
+    | {
+        name: string;
+        owner: string;
+        task_count: number;
+        history_count: number;
+      }
+    | undefined;
+  const taskRows = rows[1] as {
+    id: number;
+    name: string;
+    description: string;
+    archived: boolean;
+    completed: boolean;
+  }[];
+  if (projectRow == undefined) throw "couldn't find project " + projectId;
+  return {
+    id: projectId,
+    name: projectRow.name,
+    historyCount: projectRow.history_count,
+    taskCount: projectRow.task_count,
+    owner: projectRow.owner,
+    tasks: taskRows.map((row) => {
+      return {
+        name: row.name,
+        id: row.id,
+        archived: row.archived,
+        description: row.description,
+        completed: row.completed,
+      };
+    }),
+  };
 }
 
 export const POST = handleClientPostReq<AffectProject>(
-	affectProjectSchema,
-	async (db, session, req) => {
-		if (!(await isOfProject(db, session.email, req.projectId)))
-			return new Response("not of project", { status: 400 });
-		return Response.json(
-			await getProject(db, req.projectId),
-		);
-	},
+  affectProjectSchema,
+  async (db, session, req) => {
+    if (!(await isOfProject(db, session.email, req.projectId)))
+      return new Response("not of project", { status: 400 });
+    return Response.json(await getProject(db, req.projectId));
+  },
 );
