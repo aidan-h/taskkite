95c96a144ca86c65f64772ee427c9ffea262dd82
aidan-h <aidanhammond2003@gmail.com>
2024-02-14 08:47:33 -0600

    Added sync status

--- next-app/app/_components/SyncStatus.tsx
+++ next-app/app/_components/SyncStatus.tsx
@@ -1,22 +1,18 @@
 "use client";
 import { SyncState } from "../_lib/sync";
-import { syncStateText } from "../_lib/syncSlice";
-import SecondaryButton from "./SecondaryButton";
+import useProjects from "../_lib/useProjects";
 
-export default function SyncStatus({
-	status,
-}: {
-	status: SyncState;
-}) {
-	if (status == SyncState.SYNCING)
-		return (
-			<div className="absolute left-0 bottom-0 m-4">
-				<div>Syncing</div>
-			</div>
-		);
-	return (
-		<div className="absolute left-0 bottom-0 m-4">
-			<SecondaryButton onClick={() => console.log("sync")}>{syncStateText(status)}</SecondaryButton>
-		</div>
-	);
+function N({ syncState, name }: { syncState: SyncState, name: string }) {
+	return <div>
+		syncing {name} {syncState == SyncState.FAILED ? "failed" : ""}
+	</div>
 }
+
+export default function SyncStatus() {
+	const projects = useProjects();
+	return <div className="absolute right-0 bottom-0">
+		{projects.filter((project) => project.state != SyncState.SYNCED).map((project) =>
+			<N key={project.client.data.id} name={project.client.data.name} syncState={project.state} />)}
+	</div>
+}
+
